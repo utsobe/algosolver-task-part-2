132141e561d208dfef4667c36d49cc8d15354c8b
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-06-05 19:39:23 +0600

    header added

---
+++ src/pages/Home/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h2 className='text-2xl text-primary'>Banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/pages/Shared/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = ({ children }) => {
+    const menuItems = <>
+        <li><NavLink className='mx-1 rounded-lg' to='/'>Home</NavLink></li>
+        <li><NavLink className='mx-1 rounded-lg' to='/services'>Services</NavLink></li>
+        <li><NavLink className='mx-1 rounded-lg' to='/plans'>Plans</NavLink></li>
+        <li><NavLink className='mx-1 rounded-lg' to='/blog'>Blog & News</NavLink></li>
+        <li><NavLink className='mx-1 rounded-lg' to='/contact'>Contact</NavLink></li>
+        <li><NavLink className='mx-1 rounded-lg' to='/signin'>Sign in</NavLink></li>
+    </>
+    return (
+        <div class="drawer drawer-end">
+            <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content flex flex-col">
+                {/* <!-- Navbar --> */}
+                <div class="max-w-7xl mx-auto navbar">
+                    <div class="flex-1 px-2 mx-2">Navbar Title</div>
+                    <div class="flex-none lg:hidden">
+                        <label for="my-drawer-3" class="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
+                    </div>
+                    <div class="flex-none hidden lg:block">
+                        <ul class="menu menu-horizontal">
+                            {/* <!-- Navbar menu content here --> */}
+                            {menuItems}
+                        </ul>
+                    </div>
+                </div>
+                {/* <!-- Page content here --> */}
+                {children}
+            </div>
+            <div class="drawer-side">
+                <label for="my-drawer-3" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+                    {/* <!-- Sidebar content here --> */}
+                    {menuItems}
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
